dba3fd755b051702b109cfaad1bd379c41c602d3
prenl <elnur.kazakhstan@gmail.com>
2023-04-26 10:39:17 +0600

    add(MyHashTable): HashNode class

--- assignment3/MyHashTable.cs
+++ assignment3/MyHashTable.cs
@@ -2,5 +2,28 @@
 
 public class MyHashTable<K, V>
 {
+    private class HashNode<K, V>
+    {
+        private K _key;
+        private V _value;
+
+        public HashNode(K key, V value)
+        {
+            this._key = key;
+            this._value = value;
+        }
+
+        public override string ToString()
+        {
+            return "{ " + _key + " " + _value + " }";
+        }
+    }
+
+    private HashNode<K, V>[] _chainArray;
+    private int _m = 11;
+    private int size;
     
+    
+
+
 }
