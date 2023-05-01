5ba1bd98ddecb536fc1284d15ac6834d91712276
prenl <elnur.kazakhstan@gmail.com>
2023-05-01 14:25:10 +0600

    add(MyHashTable): Remove method

--- assignment3/MyHashTable.cs
+++ assignment3/MyHashTable.cs
@@ -6,23 +6,25 @@ public class MyHashTable<K, V>
     private class MyHashNode<K, V>
     {
         public K Key;
+
         public V Value;
+
         // Reference to the next node
         public MyHashNode<K, V>? Next;
-        
+
         // Constructor that takes key/value and put inside node
         public MyHashNode(K key, V value)
         {
             Key = key;
             Value = value;
         }
-        
+
         public override string ToString()
         {
             return "{ " + Key + " " + Value + " }";
         }
     }
-    
+
     private MyHashNode<K, V>[] _chainArray; // Chain of buckets
     private int M = 11; // Default amount of buckets
     private int _size; // Amount of items in whole HashTable
@@ -34,7 +36,7 @@ public class MyHashTable<K, V>
         _chainArray = new MyHashNode<K, V>[M]; // Create M amounts of bucket
         _size = 0;
     }
-    
+
     // Default constructor creates 11 buckets
     public MyHashTable() : this(11)
     {
@@ -57,7 +59,7 @@ public class MyHashTable<K, V>
         _chainArray[index] = newNode; // Set new node as head of bucket
         _size++; // Increment size
     }
-    
+
     // Method takes key return value
     public V? Get(K key)
     {
@@ -68,15 +70,56 @@ public class MyHashTable<K, V>
         while (temp != null)
         {
             // Check if key equals to given
-            if (temp.Key.Equals(key)) 
+            if (temp.Key.Equals(key))
                 return temp.Value; // Return Value if found
-            
+
             temp = temp.Next;
         }
-        
+
         // Return default if not found
         return default;
     }
 
-    
+    // Method takes key to remove from HashTable and return value
+    public V? Remove(K key)
+    {
+        int index = Hash(key); // Find index of bucket
+        MyHashNode<K, V>? current = _chainArray[index]; // Take head of list as temp
+        MyHashNode<K, V>? prev = null;
+
+        // Iterate through list while key is not equal to given
+        while (current != null)
+        {
+            // Check if key equals to given
+            if (current.Key.Equals(key))
+            {
+                // Check if previous is null
+                if (prev.Equals(null)) 
+                {
+                    _chainArray[index] = current.Next; // Set current's next as head of bucket
+                } 
+                // If next is null
+                else if (current.Next == null)
+                {
+                    prev.Next = null; // Set previous' next reference as null
+                }
+                // If previous and next are not null
+                else 
+                {
+                    prev.Next = current.Next; // Set previous' next reference to current's next
+                }
+                
+                _size--; // Decrement the size of table
+                return current.Value; // Return value of removed key
+            }
+            
+            prev = current;
+            current = current.Next;
+        }
+
+        // Return default (null) if not found
+        return default;
+    }
+
+
 }
