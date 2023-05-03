e5570731636395457f37e15a55f7a8ac327a3183
prenl <elnur.kazakhstan@gmail.com>
2023-05-03 10:39:11 +0600

    init assignment4

---
+++ assignment4/MyBinarySearchTree.cs
@@ -0,0 +1,19 @@
+﻿namespace assignment4;
+
+public class MyBinarySearchTree<T> where T : IComparable<T>
+{
+    private class MyNode<T> where T : IComparable<T>
+    {
+        public T Data;
+        public MyNode<T> Left;
+        public MyNode<T> Right;
+        
+        public MyNode(T data)
+        {
+            Data = data;
+        }
+    }
+
+    private MyNode<T> _root;
+    private int _size;
+}
