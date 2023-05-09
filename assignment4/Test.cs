d95906ca722854bc79eaf81ef927640940a88fe7
prenl <elnur.kazakhstan@gmail.com>
2023-05-09 17:48:28 +0600

    feat: Test class

---
+++ assignment4/Test.cs
@@ -0,0 +1,27 @@
+﻿namespace assignment4;
+
+public class Test
+{
+    public static void Main(string[] args)
+    {
+        MyBinarySearchTree<int, string> tree = new MyBinarySearchTree<int, string>();
+        
+        tree.Put(1, "one");
+        tree.Put(3, "three");
+        tree.Put(0, "zero");
+        tree.Put(12, "twelve");
+        tree.Put(7, "seven");
+        tree.Put(11, "eleven");
+        
+        tree.Delete(11);
+        
+        Console.WriteLine("Empty? " + tree.Empty());
+        
+        Console.WriteLine("1 : " + tree.Get(1));
+
+        foreach (int key in tree)
+        {
+            Console.WriteLine(key);
+        }
+    }
+}
