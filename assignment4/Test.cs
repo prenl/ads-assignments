ed4a263714360e88f1e58ffb066989376034b15a
prenl <elnur.kazakhstan@gmail.com>
2023-05-12 22:31:28 +0600

    Update Test.cs

--- assignment4/Test.cs
+++ assignment4/Test.cs
@@ -2,33 +2,39 @@
 
 public class Test
 {
+    public static void FillTree(MyBinarySearchTree<int, string> tree) 
+    {
+        Console.WriteLine("> Filling the tree...");
+        tree.Put(0, "zero");
+        tree.Put(11, "eleven");
+        tree.Put(3, "three");
+        tree.Put(13, "13teen");
+        tree.Put(7, "seven");
+        tree.Put(12, "eleven");
+        Console.WriteLine("> Tree filled.");
+    }
+
     public static void Main(string[] args)
     {
         MyBinarySearchTree<int, string> tree = new MyBinarySearchTree<int, string>();
         
         Console.WriteLine("> Empty? " + tree.Empty());
 
-        Console.WriteLine("> Filling the tree...");
-        tree.Put(0, "zero");
-        tree.Put(3, "one");
-        tree.Put(1, "three");
-        tree.Put(13, "13teen");
-        tree.Put(7, "seven");
-        tree.Put(11, "eleven");
-        Console.WriteLine("> Tree filled.");
-        
-        Console.WriteLine("> Empty? " + tree.Empty());
-        Console.WriteLine();
+        FillTree(tree);
+
+        Console.WriteLine("\n> Empty? " + tree.Empty());
         Console.WriteLine("> Maximum value => " + tree.Max());
         Console.WriteLine("> Minimum value => " + tree.Min());
-        Console.WriteLine();
+        tree.Delete(3);
+        Console.WriteLine("> Key 3 deleted");
 
-        Console.WriteLine("> All values => ...");
+        Console.WriteLine("\n> All values => ...");
         foreach (KeyValuePair<int, string> keyvalue in tree)
         {
-            Console.WriteLine(keyvalue);
+            Console.WriteLine("> " + keyvalue);
         }
 
-        Console.WriteLine("> Size: " + tree.Size());
+        Console.WriteLine("\n> Items: " + tree.Size());
+        Console.WriteLine("");
     }
 }
