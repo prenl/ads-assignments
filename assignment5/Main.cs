53871e9065e6c0b377837c048ae25c825b34b70a
prenl <elnur.kazakhstan@gmail.com>
2023-05-19 15:08:45 +0600

    feat(Sorter): Test Runner

---
+++ assignment5/Main.cs
@@ -0,0 +1,17 @@
+﻿using assignment5;
+
+using NUnit.Framework;
+using NUnitLite;
+
+namespace assingment5;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        var testAssembly = typeof(Program).Assembly;
+        var testRunner = new AutoRun(testAssembly);
+        var testResult = testRunner.Execute(args);
+        return testResult;
+    }
+}
