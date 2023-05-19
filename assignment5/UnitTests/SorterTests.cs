bad4fcd94f1fb39bc063a1e69b9e1000462e4475
prenl <elnur.kazakhstan@gmail.com>
2023-05-19 15:08:14 +0600

    feat(Sorter): Sorter Unit Tests

--- assignment5/UnitTests/SorterTests.cs
+++ assignment5/UnitTests/SorterTests.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using assingment5;
+using NUnit.Framework;
 
-namespace assignment5.UnitTests
+namespace assignment5;
+
+[TestFixture]
+public class SorterTests
 {
-    internal class SorterTests
+    private readonly Sorter _sorter = new Sorter();
+
+    [Test]
+    public void MergeSorting()
     {
+        int[] arr = { 9, 8, 7, 6, 5, 4 };
+        _sorter.MergeSort(arr);
+        Assert.AreEqual(new int[] {4, 5, 6, 7, 8, 9} , arr);
     }
+
+    [Test]
+    public void MergeSorting2() 
+    {
+        int[] arr = { 23, 46, 13, 24, 7, 1 };
+        _sorter.MergeSort(arr);
+        Assert.AreEqual(new int[] { 1, 7, 13, 23, 24, 46 }, arr);
+    }
+
+    [Test]
+    public void MergeSorting3() 
+    {
+        int[] arr = { -1, -3, 0, 54, 23 };
+        _sorter.MergeSort(arr);
+        Assert.AreEqual(new int[] { -1, -3, 0, 54, 23 }, arr);
+    }
+
 }
