fe3226dfaf19242226fd6e37120ddfae24655850
prenl <elnur.kazakhstan@gmail.com>
2023-05-19 16:09:16 +0600

    feat(Sorter): QuickSort

--- assignment5/assignment5.cs
+++ assignment5/assignment5.cs
@@ -3,7 +3,9 @@
 public class Sorter
 {
 
-    public void MergeSort(int[] arr) 
+    // Merge Sort
+
+    public void MergeSort(int[] arr)
     {
         MergeSort(arr, 0, arr.Length - 1);
     }
@@ -19,8 +21,8 @@ public class Sorter
 
         for (int i = 0; i < mid; i++)
             left[i] = arr[i];
-        
-        for (int i = mid; i < arr.Length; i++) 
+
+        for (int i = mid; i < arr.Length; i++)
             right[i - mid] = arr[i];
 
         MergeSort(left, 0, left.Length - 1);
@@ -47,6 +49,42 @@ public class Sorter
         while (j < right.Length)
             arr[k++] = right[j++];
 
+    }
+
+
+    // QuickSort
+
+    public void QuickSort(int[] arr)
+    {
+        QuickSort(arr, 0, arr.Length - 1);
+    }
+
+    private void QuickSort(int[] arr, int start, int end)
+    {
+        if (start < end)
+        {
+            int pi = Partition(arr, start, end);
+            QuickSort(arr, start, pi - 1);
+            QuickSort(arr, pi, end);
+        }
+    }
+
+    private int Partition(int[] arr, int start, int end)
+    {
+        int pivot = arr[end];
+        int i = start - 1;
+
+        for (int j = start; j < end; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                (arr[i], arr[j]) = (arr[j], arr[i]);
+            }
+        }
+
+        (arr[i+1], arr[end]) = (arr[end], arr[i+1]);
+        return (i + 1);
     }
 
 }
