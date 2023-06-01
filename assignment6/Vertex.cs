1f2641a9a3fe0a523280eee88ce80079d98a8531
prenl <elnur.kazakhstan@gmail.com>
2023-06-01 14:27:22 +0600

    Create Vertex.cs

---
+++ assignment6/Vertex.cs
@@ -0,0 +1,37 @@
+﻿namespace assignment6;
+
+public class Vertex<V>
+{
+
+    private V Data;
+    private Dictionary<Vertex<V>, double> AdjacentVertices;
+
+    public Vertex(V data) {
+        Data = data;
+        AdjacentVertices = new Dictionary<Vertex<V>, double>();
+    }
+
+    public void AddAdjacentVertex(Vertex<V> vertex, double weight)
+    {
+        AdjacentVertices.Add(vertex, weight);
+    }
+
+    public void DeleteAdjacentVertex(Vertex<V> vertex) 
+    {
+        bool delete = AdjacentVertices.Remove(vertex);
+
+        if (!delete)
+        {
+            foreach(var v in AdjacentVertices.Keys)
+            {
+                if(vertex.Data.Equals(v.Data))
+                {
+                    AdjacentVertices.Remove(v);
+                }
+            }
+        }
+    }
+
+
+
+}
