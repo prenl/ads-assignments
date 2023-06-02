905073fb4a7dcb124c8e761fc9d8927cf8a4f340
prenl <elnur.kazakhstan@gmail.com>
2023-06-02 15:55:13 +0600

    add: DFS implementation

---
+++ assignment6/Search/DepthFirstSearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment6.Search;
+
+public class DepthFirstSearch<Vertex> : Search<Vertex>
+{
+
+    public DepthFirstSearch(WeightedGraph<Vertex> graph, Vertex source) : base(source)
+    {
+        dfs(graph, source);
+    }
+
+    private void dfs(WeightedGraph<Vertex> graph, Vertex source)
+    {
+        Marked.Add(source);
+        Count++;
+
+        foreach(Vertex v in graph.AdjacencyList(source))
+        {
+            if (!Marked.Contains(v))
+            {
+                EdgeTo.Add(v, source);
+                dfs(graph, v);
+            }
+        }
+    }
+
+}
