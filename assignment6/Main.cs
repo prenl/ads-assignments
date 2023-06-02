87b9be493ddf5ff9748900ff40db8cdfba578823
prenl <elnur.kazakhstan@gmail.com>
2023-06-02 15:50:40 +0600

    Update Main.cs

--- assignment6/Main.cs
+++ assignment6/Main.cs
@@ -11,14 +11,25 @@ public class Program
 
         graph.AddEdge("Almaty", "Astana", 2.1);
         graph.AddEdge("Almaty", "Shymkent", 7.2);
-        graph.AddEdge("Shymkent", "Astana", 3.9);
+        graph.AddEdge("Astana", "Shymkent", 3.9);
         graph.AddEdge("Almaty", "Kyzylorda", 27);
         graph.AddEdge("Shymkent", "Kyzylorda", 5.4);
 
-        DijkstraSearch<string> search = new DijkstraSearch<string>(graph, "Almaty");
+        BreadthFirstSearch<string> search = new BreadthFirstSearch<string>(graph, "Almaty");
+        outputPath(search, "Shymkent");
         
-        foreach(var item in search.GetPathTo("Kyzylorda")) {
-            Console.Write(item + " <--> ");
+    }
+
+    public static void outputPath(Search<string> search, String key)
+    {
+        var list = search.GetPathTo(key);
+        int i = 0;
+
+        foreach (var item in list)
+        {
+            Console.Write(item);
+            if (!(i == list.Count() - 1)) Console.Write(" <--> ");
+            i++;
         }
     }
 
