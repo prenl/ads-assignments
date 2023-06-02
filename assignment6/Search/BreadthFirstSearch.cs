c94d209082caccf0d2b75f76873c82bb43d8117a
prenl <elnur.kazakhstan@gmail.com>
2023-06-02 15:50:33 +0600

    add: BFS implementation

---
+++ assignment6/Search/BreadthFirstSearch.cs
@@ -0,0 +1,34 @@
+﻿namespace assignment6.Search;
+
+public class BreadthFirstSearch<Vertex> : Search<Vertex>
+{
+
+    public BreadthFirstSearch(WeightedGraph<Vertex> graph, Vertex source) : base(source)
+    {
+        bfs(graph, source);
+    }
+
+    private void bfs(WeightedGraph<Vertex> graph, Vertex source)
+    {
+        Marked.Add(source);
+        LinkedList<Vertex> queue = new LinkedList<Vertex>();
+        queue.AddLast(source);
+
+        while (queue.Count != 0)
+        {
+            Vertex v = queue.First.Value;
+            queue.RemoveFirst();
+
+            foreach (Vertex vertex in graph.AdjacencyList(v))
+            {
+                if (!Marked.Contains(vertex))
+                {
+                    Marked.Add(vertex);
+                    EdgeTo.Add(vertex, v);
+                    queue.AddLast(vertex);
+                }
+            }
+        }
+    }
+
+}
